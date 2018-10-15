a1433815725b74fbee7a66864534eb4081a49cf6
luiskiller99 <43623649+luiskiller99@users.noreply.github.com>
2018-10-14 21:23:24 -0600

    main mejorado

---
+++ Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.xml.stream.events.EndElement;
+
+public class Main extends Application {
+    @Override
+
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("Tabla periódica.fxml"));
+        primaryStage.setTitle("Tabla periodica de lo elementos");
+        primaryStage.setScene(new Scene(root, 1376, 615));
+        primaryStage.show();
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
